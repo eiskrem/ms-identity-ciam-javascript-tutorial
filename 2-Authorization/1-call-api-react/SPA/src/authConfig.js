cb1a85eb557b0f53beb84b9e05ccd71e5f3d3e95
Robin Meyer <robin.meyer@icomcept.de>
2024-08-02 10:58:11 +0200

    funktioniert

--- 2-Authorization/1-call-api-react/SPA/src/authConfig.js
+++ 2-Authorization/1-call-api-react/SPA/src/authConfig.js
@@ -11,45 +11,46 @@ import { LogLevel } from "@azure/msal-browser";
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
  */
 export const msalConfig = {
-    auth: {
-        clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://Enter_the_Tenant_Subdomain_Here.ciamlogin.com/', // Replace the placeholder with your tenant subdomain
-        redirectUri: '/', // You must register this URI on Microsoft Entra admin center/App Registration. Defaults to window.location.origin
-        postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
+  auth: {
+    clientId: "7f1ba745-de60-4cf6-9d42-a747f0f45da2", // This is the ONLY mandatory field that you need to supply.
+    authority:
+      "https://login.microsoftonline.com/6c0df916-8012-4746-b574-ce47ffb6bffa/", // Replace the placeholder with your tenant subdomain
+    redirectUri: "http://localhost:3000/", // You must register this URI on Microsoft Entra admin center/App Registration. Defaults to window.location.origin
+    postLogoutRedirectUri: "/", // Indicates the page to navigate after logout.
+  },
+  cache: {
+    cacheLocation: "localStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      /**
+       * Below you can configure MSAL.js logs. For more information, visit:
+       * https://docs.microsoft.com/azure/active-directory/develop/msal-logging-js
+       */
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
     },
-    cache: {
-        cacheLocation: 'localStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
-        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-    },
-    system: {
-        loggerOptions: {
-            /**
-             * Below you can configure MSAL.js logs. For more information, visit:
-             * https://docs.microsoft.com/azure/active-directory/develop/msal-logging-js
-             */
-            loggerCallback: (level, message, containsPii) => {
-                if (containsPii) {
-                    return;
-                }
-                switch (level) {
-                    case LogLevel.Error:
-                        console.error(message);
-                        return;
-                    case LogLevel.Info:
-                        console.info(message);
-                        return;
-                    case LogLevel.Verbose:
-                        console.debug(message);
-                        return;
-                    case LogLevel.Warning:
-                        console.warn(message);
-                        return;
-                    default:
-                        return;
-                }
-            },
-        },
-    },
+  },
 };
 
 /**
@@ -57,13 +58,13 @@ export const msalConfig = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const protectedResources = {
-    toDoListAPI: {
-        endpoint: 'https://localhost:44351/api/todolist',
-        scopes: {
-            read: ['api://Enter_the_Web_Api_Application_Id_Here/ToDoList.Read'],
-            write: ['api://Enter_the_Web_Api_Application_Id_Here/ToDoList.ReadWrite'],
-        },
+  toDoListAPI: {
+    endpoint: "https://localhost:44351/api/todolist",
+    scopes: {
+      read: ["api://7f1ba745-de60-4cf6-9d42-a747f0f45da2/Plate-App.Read"],
+      write: ["api://7f1ba745-de60-4cf6-9d42-a747f0f45da2/Plate-App.ReadWrite"],
     },
+  },
 };
 
 /**
@@ -73,5 +74,8 @@ export const protectedResources = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: [...protectedResources.toDoListAPI.scopes.read, ...protectedResources.toDoListAPI.scopes.write],
+  scopes: [
+    ...protectedResources.toDoListAPI.scopes.read,
+    ...protectedResources.toDoListAPI.scopes.write,
+  ],
 };
